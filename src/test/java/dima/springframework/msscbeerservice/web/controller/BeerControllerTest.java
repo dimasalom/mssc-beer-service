074b4e50a33f3a182f21353b77379512aced670d
Dmytro.Salomakhin <salomakhin.dmytro@gmail.com>
2022-03-20 19:43:32 +0200

    Created JUnit 5 test for Beer Controller. Closes #4.

---
+++ src/test/java/dima/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,66 @@
+package dima.springframework.msscbeerservice.web.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dima.springframework.msscbeerservice.web.model.BeerDto;
+import dima.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static java.util.UUID.randomUUID;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    final String URL_TEMPLATE = "/api/v1/beer/";
+
+
+    @Test
+    void getBeerById() throws Exception {
+        mockMvc.perform(get(URL_TEMPLATE + randomUUID())
+                .accept(APPLICATION_JSON))
+                .andExpect(status().is(200));
+    }
+
+    @Test
+    void saveBeer() throws Exception {
+        BeerDto beerDto = BeerDto.builder()
+                .id(randomUUID())
+                .beerName("Some beer")
+                .beerStyle(BeerStyleEnum.IPA)
+                .build();
+
+        String beerDtoJonObj = mapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(post(URL_TEMPLATE)
+                .contentType(APPLICATION_JSON)
+                .content(beerDtoJonObj))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void updateBeer() throws Exception {
+        BeerDto beerDto = BeerDto.builder()
+                .id(randomUUID())
+                .beerName("Some beer")
+                .beerStyle(BeerStyleEnum.IPA)
+                .build();
+
+        String beerDtoJonObj = mapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(put(URL_TEMPLATE + randomUUID())
+                .contentType(APPLICATION_JSON)
+                .content(beerDtoJonObj))
+                .andExpect(status().isNoContent());
+    }
+}
