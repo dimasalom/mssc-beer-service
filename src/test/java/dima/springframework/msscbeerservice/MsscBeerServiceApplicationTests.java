e1c1d64e79da88bda39a2fd3b0f21f66105004df
Dmytro.Salomakhin <salomakhin.dmytro@gmail.com>
2022-03-20 18:33:15 +0200

    Created project. Closes #1.

---
+++ src/test/java/dima/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dima.springframework.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
