41f2e93417739049e208da5086040dbc3c688eba
Dmytro.Salomakhin <salomakhin.dmytro@gmail.com>
2022-08-04 12:07:10 +0300

    added valitation to beer dto closes #7.

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -1,10 +1,11 @@
 package guru.springframework.msscbeerservice.web.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,12 +15,29 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
+
+    @Null
     private OffsetDateTime createdData;
+
+    @Null
     private OffsetDateTime  lastModifiedData;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
     private Long upc;
+
+
     private BigDecimal price;
 }
